501b3fd1198d56be4f095f368d2a4a06156c99d3
Haicheng Charles Zhao <czhao39@gmail.com>
2016-07-26 10:10:35 -0400

    return closest distance rather than average for objects

--- scripts/objectDetector.py
+++ scripts/objectDetector.py
@@ -16,31 +16,33 @@ class ObjectDetector:
         angles = []
         dists = []
         running_sum = count_object = count_far = 0
+        min_object = 100000  # closest point of object
         for i, r in enumerate(msg.ranges):
             if r < 5 and (not count_object or abs(r - running_sum/count_object) < 0.1):  # if same object or new object
                 running_sum += r
                 count_object += 1
                 count_far = 0
+                if r < min_object: min_object = r
             else:
                 count_far += 1
                 if count_far == 15 and count_object:
-                    avg_r = running_sum / count_object
+                    #avg_r = running_sum / count_object
                     #width = count_close / 1080 * 3/2 * math.pi * avg_r  # arc length
                     #if width > .005:  # if width of object larger than 1 cm
                     if count_object > 15:  # detected an actual object
                         widths.append(count_object / 4)
                         angles.append((i + count_object/2) / 4)
-                        dists.append(avg_r)
+                        dists.append(min_object)
                     running_sum = 0
                     count_object = 0
+                    min_object = 100000
         if count_object:  # check last window
-            avg_r = running_sum / count_object
             #width = count_close / 1080 * 3/2 * math.pi * avg_r  # arc length
             #if width > .005:  # if width of object larger than 1 cm
             if count_object > 15:
                 widths.append(count_object / 4)
                 angles.append((i + count_objects/2) / 4)
-                dists.append(avg_r)
+                dists.append(min_object)
         if widths:  # only publish if objects detected
             detections = ObjectDetections()
             detections.widths = widths  # in degrees
